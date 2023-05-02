d305ad10fbe1e7f0759da2d4e5719d157951f77c
Jiamin-Zou <jiaminz622@gmail.com>
2023-05-02 18:17:15 -0400

    Completed Browser TTT

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -1,5 +1,11 @@
-Add your import statements for View and Game here
+// Add your import statements for View and Game here
+import View from "./ttt-view";
+import Game from "../ttt_node/game";
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  const figure = document.querySelector(".ttt")
+  const game = new Game();
+  // debugger
+  const view = new View(game, figure);
 });
--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,17 +1,82 @@
+// import Game from "../ttt_node/game"
+import MoveError from "../ttt_node/moveError";
+
 class View {
   constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    this.handleClick = this.handleClick.bind(this)
+    this.setupBoard();
   }
   
   setupBoard() {
+    const grid = document.createElement("ul");
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const spot = document.createElement("li");
+        spot.dataset.pos = `[${i},${j}]`;
+        // spot.dataset.pos = JSON.stringify([i, j])
+        // spot.innerText = `${[i, j]}`
+        grid.appendChild(spot)
+      }
+    }
+    this.el.appendChild(grid);
+    const text = document.createElement("p");
+    text.classList.add("message")
+    text.innerText = `Welcome to Tic-Tac-Toe\nCurrent Turn: '${this.game.currentPlayer.toUpperCase()}'`
+    this.el.appendChild(text)
+    document.addEventListener("click", this.handleClick)
   }
   
   handleClick(e) {
+    const triggerEl = e.target
+    // debugger
+    this.makeMove(triggerEl)
   }
 
   makeMove(square) {
+    // debugger
+    const player = this.game.currentPlayer;
+    const pos = JSON.parse(square.dataset.pos)
+    const msg = document.querySelector(".message");
+    try {
+      this.game.playMove(pos);
+    } catch (e) {
+      if (e instanceof MoveError) {
+        alert(e.msg);
+        this.game.playMove(pos)
+      } else {
+        throw e;
+      }
+    }
+    square.classList.add(player);
+    square.innerText = player;
+    // debugger
+    msg.innerText = `Current Turn: '${this.game.currentPlayer.toUpperCase()}'`
+    if (this.game.isOver()) {
+      document.removeEventListener("click", this.handleClick)
+      this.handleGameOver(msg);
+    }
   }
   
-  handleGameOver() {
+  handleGameOver(msg) {
+    const winner = this.game.winner();
+    // debugger
+    if (winner) {
+      msg.innerText = `Player '${winner.toUpperCase()}' has won!\n`;
+      const winMark = document.getElementsByClassName(winner);
+      Object.values(winMark).forEach(li => {
+        li.classList.add("winner");
+      })
+      // debugger
+    } else {
+      msg.innerText = 'IT WAS A TIE,\n NO ONE WINS!\n';
+    }
+    const replay = document.createElement("button");
+    replay.innerText = "Play Again !";
+    replay.classList.add("replay-btn");
+    msg.appendChild(replay);
+    replay.addEventListener("click", e => location.reload())
   }
 }
 
